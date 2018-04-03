7db4dcc62d0995e39628ae191571fc54981ad683
Mahdi Yari <m.yary15@gmail.com>
2018-04-03 21:41:45 +0430

    Express server

---
+++ index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+app.use('/',express.static(__dirname +'/'));
+app.listen(3000,'0.0.0.0',function(){
+  console.log('server started on port 3000');
+});
